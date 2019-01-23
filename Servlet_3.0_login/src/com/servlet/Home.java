08149be0e39e010779dc436fe43cef3b81874429
760943 <760943@PC182779.cts.com>
2019-01-23 10:52:48 +0530

    This is my handson in Servlet 3.0 Login

---
+++ Servlet_3.0_login/src/com/servlet/Home.java
@@ -0,0 +1,43 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		 PrintWriter pw=res.getWriter();
+		 res.setContentType("text/html");
+		 
+		 String user1=req.getParameter("Name");
+		 String pass1=req.getParameter("Password");
+		
+		pw.println("Name"+user1);
+		pw.println("Password"+pass1);
+	}
+
+}
